0070b1b1a88adc0b417897dd63e257a96a48cc8f
Володин Михаил Андреевич <ransoone@gmail.com>
2024-03-06 14:03:46 +0300

    Update Actions.java

--- gatling-maven-plugin-demo-java-main/src/test/java/NameService/Actions.java
+++ gatling-maven-plugin-demo-java-main/src/test/java/NameService/Actions.java
@@ -10,7 +10,7 @@ public class Actions {
             exec(http("name")
                     .get("/weather-moscow-4368/#{when}/ ")
                     .header("content-type", "application/json; charset = utf-8")
-                    .check(status().is(200),
-                        jsonPath("$..id").findAll().optional().saveAs("id"))
+                    .check(status().is(200).name("Код ответа не = 200"),
+                        jsonPath("$..id").findAll().saveAs("id").name("jsonPath не нашел данных"))
             );
 }
